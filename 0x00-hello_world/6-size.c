5676cb9d1268d523a5d9af8221185262d9c71e68
Mistroo35 <musasaifaldeen35@gmail.com>
2023-09-07 14:57:40 -0700

    h

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using sizof to print the size of various tybes.
+ *
+ * Return 0 (success)
+*/
+
+int main(void) 
+{
+	printf("Size of a char: %lu byte(s)\n". sizeof(char));
+	printf("siza of an int: %lu byte(s)\n". sizeof(int));
+	printf("siza of a long int: %lu byte(s)\n". sizeof(long int));
+	printf("siza of a long long int: %lu byte(s)\n". sizeof(long long int));
+	printf("siza of a float: %lu byte(s)\n". sizeof(float));
+	return (0);
+}
