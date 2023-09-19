a3dd0543fe49d5e3d825d17f3c9633ba846ccf96
Mistroo35 <musasaifaldeen35@gmail.com>
2023-09-19 08:42:43 -0700

    h

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,6 +5,9 @@
 
 void reset_to_98(int *n);
 {
-
-	*n = 98;
+	int number = 42
+		int *ptr = &number;
+	printf("Before function call: %d\n", *ptr);
+		printf("After function call: %d\n", *ptr);
+	return 0;
 }
