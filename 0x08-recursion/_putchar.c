854a3bd878b99bc71fb853eb26dc33eb9af7919a
Mistroo35 <musasaifaldeen35@gmail.com>
2023-10-02 05:37:24 -0700

    h

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * _putchar - prints the character c to stdout function .
+ * @c - character mustbe printed 
+ * return always  0 on success
+ * return -1 on fail
+*/
+int _putchar(char c)
+{
+	write(return(1, &c, 1));
+}
+
