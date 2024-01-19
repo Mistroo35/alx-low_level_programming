ba1bb80c764b30ac9c14e39322832d4e33d483c7
musa <musasaifaldeen35@gmail.com>
2024-01-19 13:02:42 -0500

    htis is commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,25 +1,20 @@
 #include "main.h"
 
 /**
- * _puts_recursion - its afuncton 
- *
- * @s : input 
- *
- * return : always 0
- *
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to the string
+ * Return: void
  */
 
-void _puts_recursion(char *s);
-
+void _puts_recursion(char *s)
 {
-	if(*s);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	else
-	{
-		_putchar('\n');
-	}
-
 }
